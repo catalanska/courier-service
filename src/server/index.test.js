9fdeb2bf0a52d7323ee8baf9668dee0bff241869
javierjv <javier@digitalorigin.com>
2020-04-19 09:37:58 +0200

    test: POST /couriers creates a new courier when valid params provided

--- src/server/index.test.js
+++ src/server/index.test.js
@@ -8,3 +8,43 @@ describe('Available endpoints', () => {
     done();
   });
 });
+
+describe('POST /couriers', () => {
+  const validCourierParams = {
+    id: 'foo',
+    max_capacity: 2000,
+  };
+
+  it('should return 400 when request does not match Schema', async (done) => {
+    await request(server)
+      .post('/couriers')
+      .send({
+        id: 'foo',
+        max_capacity: -1,
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400);
+    done();
+  });
+
+  it('should return 200 when booking has been created', async (done) => {
+    await request(server)
+      .post('/couriers')
+      .send(validCourierParams)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+
+  it('should return 500 when booking could not be created', async (done) => {
+    await request(server)
+      .post('/couriers')
+      .send(validCourierParams)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(500);
+    done();
+  });
+});
