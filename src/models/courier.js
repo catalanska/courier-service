84310a9143048cd6b02ab62f90f0e029b98065b1
javierjv <javier@digitalorigin.com>
2020-04-19 12:40:42 +0200

    feat: Find couriers with and without maxCapacity filter

--- src/models/courier.js
+++ src/models/courier.js
@@ -4,7 +4,12 @@ import mongoose from 'mongoose';
 const courierSchema = new mongoose.Schema({
   courierId: { type: String, required: true },
   maxCapacity: { type: Number, required: true },
-  currentCapacity: { type: Number, default: 0 },
+  currentCapacity: {
+    type: Number,
+    default: function () {
+      return this.maxCapacity;
+    },
+  },
 });
 
 const courierModel = new mongoose.model('Courier', courierSchema);
